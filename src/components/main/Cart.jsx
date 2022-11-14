835923687c179c7d69512cf1c1a59bbad7a2838b
chuchushooes <chuchushooes@gmail.com>
2022-11-14 22:29:28 +0800

    頁面新增購物車區塊、學習用props map資料

---
+++ src/components/main/Cart.jsx
@@ -0,0 +1,65 @@
+import styles from '../../css/Cart.module.css'
+import { ReactComponent as ProductActionMinus} from '../icons/minus.svg'
+import { ReactComponent as ProductActionPlus} from '../icons/plus.svg'
+
+const productItems = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  }
+];
+
+function ProductList () {
+  return (
+    <>
+    {productItems.map((item) => (
+        <div key={item.id}  className={`${styles.productContainer} `} data-count="0" data-price={item.price}>
+          <img className={styles.imgContainer} src={item.img} alt={item.img} />
+              <div className={styles.productInfo}>
+                <div className={styles.productName}>{item.name}</div>
+                <div className={styles.productControlContainer}>
+                  <div className={styles.productControl}>
+                    <ProductActionMinus className={styles.productAction}/>
+                    <span className={styles.productCount}>{item.quantity}</span>
+                    <ProductActionPlus className={styles.productAction}/>
+                  </div>
+                </div>
+                <div className={styles.price}>{item.price}</div>
+              </div>
+        </div>
+    ))}
+    </>
+  )
+}
+
+//require 引入image
+export function Cart () {
+  return (
+    <>
+      <h3 className={styles.cartTitle}>購物籃</h3>
+        <section className={`${styles.cartList}`} data-total-price="0">
+          <ProductList />
+        </section>
+        <div lassName={styles.cartInfoWrap}>
+          <section className={`${styles.cartInfo} ${styles.col12} `}>
+            <div className={styles.cartInfoText}>運費</div>
+            <div className={styles.cartInfoPrice}>免費</div>
+          </section>
+          <section className={`${styles.cartInfo} ${styles.col12} `}>
+            <div className={styles.cartInfoText}>小計</div>
+            <div className={styles.cartInfoPrice}>$0</div>
+          </section>
+        </div>
+    </>
+  )
+}

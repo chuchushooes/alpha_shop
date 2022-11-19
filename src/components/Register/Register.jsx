3f8ba8246d4c97c99857c4c44dfc9ac8fd45d2c3
chuchushooes <chuchushooes@gmail.com>
2022-11-19 14:50:55 +0800

    eslink bug修正以及結帳按鈕按下時切換頁面和流程

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,18 +4,20 @@ import StepTwoShipping from './StepTwoShipping/StepTwoShipping'
 import StepThreeCreditcard from './StepThreeCreditcard/StepThreeCreditcard'
 import ProgressControl from './ProgressControl/ProgressControl'
 import styles from './Register.module.css'
+import { useState } from 'react'
 
 function Register() {
+  const [currentStep, setStep] = useState(1)
   return (
     <section className={`${styles.registerContainer} ${styles.colLg6} ${styles.colSm12}`}>
       <h2 className={`${styles.registerTitle} ${styles.col12}`}>結帳</h2>
-      <StepProgress />
+      <StepProgress currentStep={currentStep} />
       <section className={styles.formContainer}>
-        <StepOneAddress />
-        <StepTwoShipping />
-        <StepThreeCreditcard />
+        {currentStep === 1 && <StepOneAddress />}
+        {currentStep === 2 && <StepTwoShipping />}
+        {currentStep === 3 && <StepThreeCreditcard />}
       </section>
-      <ProgressControl />
+      <ProgressControl currentStep={currentStep} onClick={setStep} />
     </section>
   )
 }
--- src/components/Register/StepTwoShipping/StepTwoShipping.jsx
+++ src/components/Register/StepTwoShipping/StepTwoShipping.jsx
@@ -2,7 +2,7 @@ import styles from './StepTwoShipping.module.css'
 
 function StepTwoShipping() {
   return (
-    <form className={styles.StepTwoShipping} style={{ display: 'none' }} data-phase="shipping">
+    <form className={styles.StepTwoShipping} data-phase="shipping">
       <h3 className={styles.formTitle}>運送方式</h3>
       <section className={styles.formBody}>
         <label className={styles.radioGroup} data-price="0">

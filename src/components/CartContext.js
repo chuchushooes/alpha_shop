a76f6d6cd5f10c973e1eb7b228a7abf1f95e04a6
chuchushooes <chuchushooes@gmail.com>
2022-12-08 14:48:24 +0800

    新增CartContext.js

---
+++ src/components/CartContext.js
@@ -0,0 +1,21 @@
+import { createContext } from 'react'
+
+const productItems = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 1
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1
+  }
+]
+
+// 將 Product 初始物件資料放入 context 內
+export const ProductsContext = createContext(productItems)
